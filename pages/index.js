fbaea7aa2f78e31d4313a253585a214fd7c75717
Alexandru <nedelcu_alexandru@outlook.com>
2023-04-08 23:38:19 +0200

    first commit with navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Navbar from '@components/Navbar' // import the Navbar component
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Navbar /> {/* include the Navbar component */}
 
       <main>
         <Header title="Welcome to my app!" />
